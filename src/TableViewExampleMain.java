ab994304b3a82f8a2c5e805f75a8c3013c88836d
Gerry <ghartlingjr@gmail.com>
2019-09-15 09:26:46 -0400

    tableview example

---
+++ src/TableViewExampleMain.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class TableViewExampleMain  {
+    public static void main(String[] args) {
+    	Application.launch(TableViewExample.class);
+    }
+
+ 
+}
