ef41620cad4975b391d28d960feee68e5f4a7ca7
ghartling <gerald.hartling@cesicorp.com>
2019-10-02 08:56:25 -0400

    proto

---
+++ src/com/hartling/fx/TableViewExampleMain.java
@@ -0,0 +1,9 @@
+package com.hartling.fx;
+
+import javafx.application.Application;
+
+public class TableViewExampleMain {
+	public static void main(String[] args) {
+		Application.launch(TableViewExample.class);
+	}
+}
